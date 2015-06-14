4290841850549e99fea8738f1626b61e767f34bf
tsuneo.kurihara@ <tsuneo.kurihara@gmail.com>
2015-06-14 12:44:09 +0200

    update

--- js/taxihack.js
+++ js/taxihack.js
@@ -1,35 +1,13 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+$(document).ready(function() {
+    $.ajax({
+        url: 'https://taxihack.stamplayapp.com/api/cobject/v1/hole',
+        method: 'GET'
+    }).then(function(data) {
+       console.log(data);
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-/*// Google Maps Scripts
+// Google Maps Scripts
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 
@@ -176,4 +154,3 @@ function init() {
         icon: image
     });
 }
-*/
